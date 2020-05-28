4b58a576e9bb0118521556b408b3ab2734d2f07d
haderman <hadermania@gmail.com>
2020-05-28 10:53:04 -0500

    Create script shell to wathc file changes

--- dev.sh
+++ dev.sh
@@ -1,25 +1,48 @@
 #!/bin/sh
 
+# how to use: sh dev.sh popup|newtab|background
+# This script is to watch changes in files and creat bundles from it
 
-FIRST_ARGUMENT="$1"
+trap before_exit EXIT
 
-echo "hola mundo: ${FIRST_ARGUMENT}"
-
-
-cd_elm_folder()
-{
-  cd src/elm
+before_exit() {
+  echo "End"
 }
 
-watch_newtab()
-{
+watch_newtab() {
+  cd src/elm
   elm-live src/NewTab.elm -- --debug --output=../newtab/newtab.bundle.js
 }
 
-watch_popup()
-{
+watch_popup() {
+  cd src/elm
   elm-live src/Popup.elm -- --debug --output=../popup/popup.bundle.js
 }
 
-cd_elm_folder
-watch_newtab
+watch_background() {
+  # this works to me to make de "denon" command available
+  # but I think this shouldn't be here. I have to research more about this
+  # 
+  # export PATH="/Users/<your account>/.deno/bin:$PATH"
+  #
+  export PATH="/Users/hadercardona/.deno/bin:$PATH"
+  denon start
+}
+
+case $1 in
+  "newtab")
+    watch_newtab
+    ;;
+  "popup")
+    watch_popup
+    ;;
+  "background")
+    watch_background
+    ;;
+  *)
+    echo "command not found"
+    ;;
+esac
+
+
+
